ae01314a72d82d9be47900a3ac39da5e2fab9859
Freezor <frieso@fh-trier.de>
2015-12-09 13:02:00 +0100

    Preference
    Working but not so perfect settings view

--- MobileProjectEstimator/app/src/main/java/com/mobileprojectestimator/mobileprojectestimator/SettingsActivity.java
+++ MobileProjectEstimator/app/src/main/java/com/mobileprojectestimator/mobileprojectestimator/SettingsActivity.java
@@ -1,15 +1,12 @@
 package com.mobileprojectestimator.mobileprojectestimator;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.support.annotation.LayoutRes;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
+import android.util.TypedValue;
 import android.view.View;
-import android.widget.LinearLayout;
 
 /**
  * Created by Oliver Fries on 09.12.2015, 10:38.
@@ -21,17 +18,41 @@ public class SettingsActivity extends PreferenceActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        addPreferencesFromResource(R.xml.preferences);
+        //getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+        toolbar.setClickable(true);
+        toolbar.setNavigationIcon(getResIdFromAttribute(this, R.attr.homeAsUpIndicator));
+        toolbar.setTitle(R.string.settings_title);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
+    private static int getResIdFromAttribute(final Activity activity, final int attr) {
+        if (attr == 0) {
+            return 0;
+        }
+        final TypedValue typedvalueattr = new TypedValue();
+        activity.getTheme().resolveAttribute(attr, typedvalueattr, true);
+        return typedvalueattr.resourceId;
+    }
     public static class MyPreferenceFragment extends PreferenceFragment
     {
         @Override
-        public void onCreate(final Bundle savedInstanceState)
-        {
+        public void onCreate(final Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
         }
     }
-
 }
